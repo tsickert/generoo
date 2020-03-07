65fd16f696d1ad8b3903be4e3aa196509df85bd8
Thomas Sickert <thomas.sickert@gmail.com>
2020-03-06 22:56:07 -0800

    Preserved Original File Permissions

--- generoo/utils.py
+++ generoo/utils.py
@@ -1,25 +1,32 @@
 import os
-import re
 import regex
 from pystache import Renderer
 
 yes_no = ['y', 'n']
 
+
 def render_template_to_directory(destination: str, template: str, parameters: dict):
-    overwrite_file(destination, renderer.render(open(template, 'r').read(), parameters))
+    try:
+        template_text = open(template, 'r').read()
+        permissions = os.stat(template).st_mode
+        overwrite_file(destination, renderer.render(template_text, parameters), permissions)
+    except UnicodeDecodeError:
+        print(f'Unable to load: {template}, may be binary file')
 
 
 def render_destination_path(destination: str, parameters: dict) -> str:
     return renderer.render(destination, parameters)
 
 
-def overwrite_file(file, content):
+def overwrite_file(file, content, permissions=None):
     directory_name = os.path.dirname(file)
     if directory_name != '':
         os.makedirs(directory_name, exist_ok=True)
     f = open(file, 'w')
     f.write(content)
     f.close()
+    if permissions:
+        os.chmod(file, permissions)
     
 
 def is_valid_input(input_response: str, validations: list) -> bool:
